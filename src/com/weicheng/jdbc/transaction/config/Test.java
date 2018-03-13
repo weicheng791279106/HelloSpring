00141b48dcd0e528e0158d6f526efbe4025fbe8f
weicheng791279106 <791279106@qq.com>
2018-03-13 13:37:53 +0800

    Update Test.java

--- src/com/weicheng/jdbc/transaction/config/Test.java
+++ src/com/weicheng/jdbc/transaction/config/Test.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
-	/**转账测试
+	/**杞处娴嬭瘯
 	 * @throws Exception */
 	@org.junit.Test
 	public void testTransfer() throws Exception{
-		/*加载xml配置文件*/
+		/*鍔犺浇xml閰嶇疆鏂囦欢*/
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/com/weicheng/jdbc/transaction/config/applicationContext.xml");
 		AccountService accountService = (AccountService) context.getBean("accountService");
 		accountService.transfer(1, 2, 1000);
